0f4bf71f88b09e9d3650681c0730feaa4f50039c
Rodney Leviton <rod@rodleviton.com>
2022-08-11 16:34:10 +1000

    Update home.spec.js

--- e2e/home.spec.js
+++ e2e/home.spec.js
@@ -4,7 +4,7 @@ test.describe("home", () => {
   test("should navigate user to the home page", async ({
     page,
   }) => {
-    await page.goto("https://rodleviton.com")
+    await page.goto(process.env.NEXT_PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000')
 
     await expect(page.locator("h1")).toContainText(
       "Rod Leviton"
